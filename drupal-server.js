0b402f1ec16496b962ca4e3e67ff1fcdc0547a59
Kevin Kaland <kevin@wizonesolutions.com>
2013-11-17 14:58:52 +0100

    Move validateEmail() into this package.

--- drupal-server.js
+++ drupal-server.js
@@ -27,3 +27,8 @@ Meteor.methods({
     return return_obj;
   }
 });
+
+this.validateEmail = function(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
